2990607811b9428a866673e774c88c76f13f2552
Craig Storer <craigstorer@gmail.com>
2019-12-20 15:08:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 def find_the_cheese(strings)
   
   cheese_types = ["cheddar", "gouda", "camembert"]
-   strings.select do |x|
-   cheese_types.include?
+   strings.include?select do |x|
+   cheese_types.
 end
 end
